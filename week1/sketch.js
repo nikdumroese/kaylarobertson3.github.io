9491acf580c7f7d18e7e76aa7af12f3f88575f46
Kayla Robertson <kayla.robertson3@gmail.com>
2016-08-31 11:23:18 -0600

    Added first sketch

---
+++ week1/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 400);
+
+  
+}
+
+function draw() {
+  background(0,255,0);
+  
+  stroke(0);
+  fill(255);
+  rect(200, 99, 75, 100);
+ 
+  stroke(3);
+  fill(0, 0, 255, 20);
+  ellipse(200, 99, 100, 100);
+  
+}
