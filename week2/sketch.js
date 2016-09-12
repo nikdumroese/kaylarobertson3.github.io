835f233c1d9fe31b53837c5299aed689040d4bd7
Kayla Robertson <kayla.robertson3@gmail.com>
2016-09-11 21:32:22 -0600

    added readme

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,52 +1,59 @@
-var y = 60;
-var z = 100;
+var y = 400;
+var z = 400;
+var c = color(255, 204, 0);
 
 
 function setup() {
-  createCanvas(1070, 500)
+  createCanvas(800, 500)
   rectMode (CENTER);
   ellipseMode (CENTER);
-  background (255,255,255);
+  background (102, 204, 255, 100);
 }
 
 function draw() {
   //the 4th value means opacity.
 
-  stroke(10);
+  //baseline
+  strokeWeight(0);
+  line(0, 500, 800, 500);
+
+  //circles
+  strokeWeight(1);
+  fill(100);
+  ellipse(500, 200, 100, 100);
+
+  noStroke();
   fill(255);
-  ellipse(500, 250, 100, 100);
+  ellipse(500, 200, 10, 10);
 
-  stroke(10);
-  fill(255);
-  ellipse(500, y, z, 100);
+  strokeWeight(1);
+  fill(0, 126, 255, 102);
+  ellipse(500, 200, 200, 200);
 
-  stroke(3);
-  fill(0, 126, 255, 102)
-  ellipse(500, 250, 200, 200);
-
-  stroke(5);
+  strokeWeight(1);
   fill(0, 300, 255, 102)
-  ellipse(500, 250, 300, 300);
+  ellipse(500, 200, 600, 600);
 
-//Head
-  fill (255);
-  ellipse(340, 130, 160, 160);
+  strokeWeight(0);
+  fill(255)
+  ellipse(500, 200, 300, 300);
 
-  //eyes
-  fill(0);
-  ellipse(321, 115, 20, 16);
-  ellipse(359, 115, 20, 16);
+  //mountains
+  fill(132, 147, 124);
+  triangle(100, 500, 200, 300, 300, 500);
 
+  fill(132, 100, 124);
+  triangle(200, 500, 300, 250, 400, 500);
+
+  fill(15, 147, 124);
+  triangle(300, 500, 450, 180, 600, 500);
+
+  //text
   textSize(60);
   fill(0, 102, 153);
   textFont("Raleway");
   text("hello", 12, 40);
   text("hello", 12, 80);
   text("hello", 12, 120);
-
-  ellipseMode(CENTER);
-  fill(0);
-  stroke(255);
-  arc(300, 100, 200, 200, HALF_PI,0);
 
 }
