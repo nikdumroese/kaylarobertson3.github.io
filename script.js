5421090525a8f46342a1811e08d9b6464884c115
kaylarobertson3 <kayla.robertson3@gmail.com>
2018-01-28 00:20:41 +0100

    new thumbs

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
         },
         error: function(error, reason) {
             console.log("error getting projects", reason);
+            $('#projects').append('<div class="project"><p class="error">Error getting projects. Try refreshing! :)</div>');
         }
     });
 })();
@@ -16,52 +17,42 @@
 
 // NAVIGATION
 $('#aboutLink').on('click', function() {
+    $(window).scrollTop(0);
     $('#work').fadeOut('fast');
     $('#about').fadeIn('fast');
+    $('#singleProject').hide();
 
 });
 
 $('#workLink').on('click', function() {
+    $(window).scrollTop(0);
     $('#about').fadeOut('fast');
     $('#work').fadeIn('fast');
+    $('#singleProject').hide();
+
 });
 
 $('#closeSingleProject').on('click', function() {
+    $(window).scrollTop(0);
     $('#singleProject').fadeOut('fast');
     $('#work').fadeIn('fast');
     $('#about').hide('');
+    $('#singleProject').hide();
+
 });
 
-
-
-// GET PROJECTS ARRAY FROM JSON
-$.ajax({
-    url: "projects.json",
-    type: "GET",
-    success: function(projects) {
-        console.log("projects", projects);
-        pushProjects(projects);
-    },
-    error: function(error, reason) {
-        console.log("error getting projects", reason);
-        $('#projects').append('<div class="project"><p class="error">Error getting projects. Try refreshing! :)</div>');
-    }
-});
 
 // PUSH PROJECTS
 function pushProjects(projects) {
     for (var proj = 0; proj < projects.length; proj++) {
         var title = projects[proj]["title"];
         var desc = projects[proj]["desc"];
-        var img = projects[proj]["img"];
+        var thumb = projects[proj]["thumb"];
         var pub = projects[proj]["pub"];
         var tech = projects[proj]["tech"];
         var id = projects[proj]["id"];
 
-        $('#projects').append('<div class="project"><img class="projectimg" src="' + img + '" alt="' + title + '"/><div class="photo-text"><h3 class="title" id="' + id + '">' + title + '</h3><div class="mobile-desc"><p class="mobileTitle">' + title + '</div>');
-
-        // Removed this
-        // <p class="desc">' + desc + '</p><p class="pub">' + pub + '</p><p class="tech">' + tech + '</p></div>
+        $('#projects').append('<div class="project"><img class="projectimg" src="' + thumb + '" alt="' + title + '"/><div class="photo-text"><h3 class="title" id="' + id + '">' + title + '</h3><div class="mobile-desc"><p class="mobileTitle">' + title + '</div>');
     }
 
 }
@@ -73,6 +64,7 @@ function showSingleProject(projects) {
     $('.title').on('click', function() {
         $('#work').fadeOut('fast');
         $('#singleProject').empty();
+        $(window).scrollTop(0);
 
         var clickedId = this.id;
         var filtered = projects.filter(function(item) {
@@ -95,16 +87,16 @@ function showSingleProject(projects) {
         var iframe = filtered[0].iframe;
 
         if (!filtered[0].img2) {
-            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a href="'+viewProject+'"<p class="viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/></div></div> ');
+            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a class="hov" href="'+viewProject+'"><p class="hov viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/></div></div> ');
 
         } else if (!filtered[0].img3) {
-            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a href="'+viewProject+'"<p class="viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/><img class="singleProjectImg" alt="'+alt2+'"src="'+clickedImg2+'"/></div></div> ');
+            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a class="hov" href="'+viewProject+'"><p class="hov viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/><img class="singleProjectImg" alt="'+alt2+'"src="'+clickedImg2+'"/></div></div> ');
         }
 
         else if (!filtered[0].img4) {
-            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a href="'+viewProject+'"<p class="viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/><img class="singleProjectImg" alt="'+alt2+'"src="'+clickedImg2+'"/><img class="singleProjectImg" alt="'+alt3+'"src="'+clickedImg3+'"/></div></div> ');
+            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a class="hov" href="'+viewProject+'"><p class="hov viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/><img class="singleProjectImg" alt="'+alt2+'"src="'+clickedImg2+'"/><img class="singleProjectImg" alt="'+alt3+'"src="'+clickedImg3+'"/></div></div> ');
         } else {
-            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a href="'+viewProject+'"<p class="viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/><img class="singleProjectImg" alt="'+alt2+'"src="'+clickedImg2+'"/><img class="singleProjectImg" alt="'+alt3+'"src="'+clickedImg3+'"/><img class="singleProjectImg" alt="'+alt4+'"src="'+clickedImg4+'"/></div></div> ');
+            $('#singleProject').append('<p class="back hov">CLOSE <i class="fa fa-times-circle" aria-hidden="true"></i></p><div class="single"><div class="singleLeftCol"><h3 class="title">'+clickedTitle+'</h3><p class="pub">'+clickedPub+'</p><p class="desc">'+clickedDesc+'</p><p class="tech">'+clickedTech+'</p><br/><a class="hov" href="'+viewProject+'"><p class="hov viewProject">View Project→</p></a></div><div class="singleRightCol"><img class="singleProjectImg" alt="'+alt+'"src="'+clickedImg+'"/><img class="singleProjectImg" alt="'+alt2+'"src="'+clickedImg2+'"/><img class="singleProjectImg" alt="'+alt3+'"src="'+clickedImg3+'"/><img class="singleProjectImg" alt="'+alt4+'"src="'+clickedImg4+'"/></div></div> ');
         }
 
         if (filtered[0].iframe) {
@@ -117,6 +109,8 @@ function showSingleProject(projects) {
             $('#singleProject').fadeOut('fast');
             $('#work').fadeIn('fast');
             $('#about').hide('');
+            $('#singleProject').hide();
+
         });
     });
 }
